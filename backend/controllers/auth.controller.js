07a15d185e2bfe35a4f9a3f013d4223022590348
Raunak Shah <shahrrs2004@gmail.com>
2024-10-06 18:02:38 +0530

    backend - worked on user models, mongo cluster and jwt signing of user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,57 @@
-export const signup = (req, res) =>{
-    res.send('signup User');
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import signUser from "../utils/tokenWorks.js";
+
+export const signup = async (req, res) =>{
+    try{
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+        
+        // Check for password and confirmPassword
+        if(password != confirmPassword){
+            return res.status(400).json({error: "Password don't match"})
+        }
+
+        // Check for existing User
+        const user = await User.findOne({username});
+        if(user){
+            return res.status(400).json({error: "username laready exists"})
+        }
+
+        // Password Hashed here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Get Profile Pictures
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+        
+        // Create New User
+        const newUser = new User({
+            fullName,
+            username,
+            password:hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        if(newUser){
+            signUser(newUser._id, res);
+
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        }else{
+            res.status(400).json({error: "Invalid user data"});
+        }
+
+    }catch(error){
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
 
 export const login = (req, res) =>{
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
-
-const app = express();
+import connectToMongoDB from './db/connect.js';
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
-app.get('/',(req, res)=>{
-    res.end("<h1>Hi</h1>")
-})
+// app.get('/',(req, res)=>{
+//     res.end("<h1>Hi</h1>")
+// })
 
 
 app.listen(PORT,()=>{
+    connectToMongoDB();
     console.log(`Listening on http://localhost:${PORT}.`)
 })
