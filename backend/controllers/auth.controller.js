3e41ff6ef5f14c9e0ae6c502364cb7b314b0b6bc
Rs07-404 <shahrrs2004@gmail.com>
2024-10-07 12:18:00 +0530

    backend - worked on login and logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,10 +54,36 @@ export const signup = async (req, res) =>{
     }
 }
 
-export const login = (req, res) =>{
-    res.send('login User');
+export const login = async (req, res) =>{
+    try{
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        signUser(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        })
+
+    }catch(error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
 
 export const logout = (req, res) =>{
-    res.send('logout User');
+    try{
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message: "Logged out successfully"})
+    }catch(error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
