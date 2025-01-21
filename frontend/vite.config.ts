89bcafb499aa64b492caaf7122c551521b996802
Raunak Shah <shahrrs2004@gmail.com>
2025-01-21 11:36:00 +0530

    made Deployable changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -66,7 +66,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
         '/api':{
-            target: "https://chat-net-five.vercel.app"
+            target: "http://localhost:5000"
         },
     },
   },
